9ef6dbddd3fa8c0374f7497948507a6ac6acfcc8
Çağatay İlhan <icagatay930@gmail.com>
2023-04-02 15:00:35 +0300

    zoro.to and anikatsu clone html
    zoro.to and anikatsu clone html

---
+++ clone/_config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "AniKatsu"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}/clone";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "xyz@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "http://anikatsu.shashanktiwar11.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
